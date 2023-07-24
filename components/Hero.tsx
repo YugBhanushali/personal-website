0f4c926b1d34d24bdf810aeedb4b6dfc2d62067d
YugBhanushali <yughbhanushali241103@gmail.com>
2023-07-24 11:05:40 +0530

    Added animation to hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,48 +1,80 @@
+'use client'
 import { DevelperStatus, HeroTitle } from "@/utils/Constants";
-import React from "react";
-import { motion } from "framer-motion";
+import React, { useEffect } from "react";
+import { motion, useAnimation, useScroll, useTransform } from "framer-motion";
 import StatusCircle from "./StatusCircle";
 import { Particles } from "./Particles";
 
+
 const Hero = () => {
+
+  const targetRef = React.useRef<HTMLDivElement | null>(null);
+  const { scrollYProgress } = useScroll({
+    target: targetRef,
+    offset: ["end end", "end start"]
+  });
 
   //changes the status of the Developer
   const { title, colour, boxShadow } = DevelperStatus("Active");
 
-  return (
-    <div className="flex flex-col justify-center items-center mt-[150px]">
-      <motion.div
-        initial={{ opacity: 0, y: 200 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.9, ease: "easeInOut" }}
-        className="flex flex-col items-center justify-center"
-      >
-        <div className="flex w-fit justify-center bg-[#ffffff] bg-opacity-[0.16] py-1 px-4 rounded-[20px] border-[1.5px] border-[#515151] backdrop-filter backdrop-blur-2xl">
-          <div className="flex justify-center items-center gap-2">
-            {/* status */}
-            <div className="flex justify-center">
-              <StatusCircle currentStatus="Active" />
-            </div>
+  const opacity = useTransform(scrollYProgress, 
+    [0.5,0.9]
+    ,[1, 0]);
+  const scale = useTransform(scrollYProgress,
+    [0.5, 0.9],
+    [1, 0.5]
+  );
 
-            {/* status type */}
-            <div className="text-[13px] sm:text-[15px]">{title}</div>
+  // make a function which return
+  function getNum(num:number) {
+    return num*0.14;
+  }
+
+  return (
+    <div ref={targetRef} className="flex flex-col justify-center items-center mt-[150px]">
+      <motion.div style={{opacity,scale}} className="flex flex-col justify-center items-center" >
+        <motion.div
+          initial={{ opacity: 0, y: 200 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 2.3, ease: "easeInOut" }}
+          className=" flex flex-col items-center justify-center"
+        >
+          <div className="flex w-fit justify-center bg-[#ffffff] bg-opacity-[0.16] py-1 px-4 rounded-[20px] border-[1.5px] border-[#515151] backdrop-filter backdrop-blur-2xl">
+            <div className="flex justify-center items-center gap-2">
+              {/* status */}
+              <div className="flex justify-center">
+                <StatusCircle currentStatus="Active" />
+              </div>
+
+              {/* status type */}
+              <div className="text-[13px] sm:text-[15px]">{title}</div>
+            </div>
           </div>
+        </motion.div>
+
+        {/* title */}
+        <div className="mt-[50px] sm:w-[500px] w-[300px] sm:text-[22px] text-[19px] justify-center text-center">
+          <motion.div className="flex flex-wrap items-center justify-center">
+            {HeroTitle.map((title, index) => {
+              console.log(scrollYProgress);
+              return (
+              <motion.span
+                key={index}
+                initial={{ opacity: 0, y: 150 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: getNum(index), ease: "easeInOut",stiffness: 100 ,spring: 0.5 }}
+              >
+                <div
+                  className="sm:mx-1 mx-[2px] sm:px-3 px-2 rounded-[20px] border-[1.5px] border-transparent cursor-pointer hover:bg-white hover:bg-opacity-[0.16] transition-all duration-300 ease-in-out hover:scale-110 hover:border-[#515151] hover:border-[1.5px]"
+                >
+                  {title}
+                </div>
+              </motion.span>
+              )
+            })}
+          </motion.div>
         </div>
       </motion.div>
-
-      {/* title */}
-      <div className="mt-[50px] sm:w-[500px] w-[300px] sm:text-[22px] text-[19px] justify-center text-center">
-        <div className="flex flex-wrap items-center justify-center">
-          {HeroTitle.map((title, index) => (
-            <motion.span
-              className="sm:mx-1 mx-[2px] sm:px-3 px-2 rounded-[20px] border-[1.5px] border-transparent cursor-pointer hover:bg-white hover:bg-opacity-[0.16] transition-all duration-300 ease-in-out hover:scale-110 hover:border-[#515151] hover:border-[1.5px]"
-              key={index}
-            >
-              {title}
-            </motion.span>
-          ))}
-        </div>
-      </div>
     </div>
   );
 };
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,30 +1,47 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import ProjectCard from './ProjectCard'
 import { projectCardData } from '@/utils/Constants'
 import SectionDivider from './SectionDivider'
 import { Particles } from './Particles'
+import { useScroll,motion, useTransform } from 'framer-motion'
 
 const Projects = () => {
 
+  const targetRef = useRef<HTMLDivElement | null>(null);
+  const { scrollYProgress } = useScroll({
+    target: targetRef,
+    offset: ["start end", "end start"]
+  });
+
+  const scale = useTransform(scrollYProgress,
+    [0.3, 0.5],
+    [1, 2]
+  );
+
+  const opacity = useTransform(scrollYProgress,
+    [0.2, 0.8],
+    [1, 0.5]
+  );
+
   return (
-    <div id='projects' className='flex flex-col gap-6 items-center mt-[100px]'>
-      <div className='flex'>
+    <div ref={targetRef} id='projects' className='flex flex-col gap-6 items-center mt-[100px]'>
+      <motion.div style={{scale,position:"sticky"}} className='flex'>
         <SectionDivider
           sectionName = 'Projects'
           className='rounded-[25px] hover:border-zinc-400 duration-300 ease-in mb-6'
         />
-      </div>
+      </motion.div>
       <div className='flex flex-wrap sm:gap-8 gap-6 justify-center '>
         {projectCardData.map((project, index) => (
-          <ProjectCard
-            key={index}
-            projectId={project.projectId}
-            projectName={project.projectName}
-            githubLink={project.githubLink}
-            projectLink={project.projectLink}
-            projectImage={project.projectImage}
-            techStack={project.techStack}
-          />
+            <ProjectCard
+              key={index}
+              projectId={project.projectId}
+              projectName={project.projectName}
+              githubLink={project.githubLink}
+              projectLink={project.projectLink}
+              projectImage={project.projectImage}
+              techStack={project.techStack}
+            />
         ))}
       </div>
     </div>
