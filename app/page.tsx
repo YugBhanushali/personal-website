32ec48bab54bdbb05b5df2de5f4cdac7c80d0e12
YugBhanushali <yughbhanushali241103@gmail.com>
2023-07-28 23:44:30 +0530

    Some changes in background

--- app/page.tsx
+++ app/page.tsx
@@ -12,14 +12,21 @@ import Techs from '@/components/Techs'
 import Tools from '@/components/Tools'
 import PreLoader from '@/components/ui/PreLoader'
 import { useRef } from 'react'
+import { isMobile } from 'react-device-detect'
 
 export default function Home() {
 
   const targetRef = useRef<HTMLDivElement>(null)
   return (
-    <main  className="flex min-h-screen flex-col items-center justify-between px-24">
-      <div ref={targetRef} className="flex flex-col min-h-screen items-center justify-between">
-          <Particles quantity={400} containerRef={targetRef} className="absolute inset-0 -z-10 w-full h-full " />
+    <main  className="flex flex-col items-center justify-between px-24">
+      <div ref={targetRef} className="flex flex-col items-center justify-between">
+          {
+            isMobile 
+            ?
+            <Particles quantity={100} containerRef={targetRef} className="absolute inset-0 -z-10 w-full h-full " />
+            :
+            <Particles quantity={400} containerRef={targetRef} className="absolute inset-0 -z-10 w-full h-full " />
+          }
           <Hero />
 
           <Projects />
