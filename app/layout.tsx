de483cda2161ed8254b52c9902ed851e33ba6ade
YugBhanushali <yughbhanushali241103@gmail.com>
2023-08-19 14:06:27 +0530

    Some changes in head part

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="icon.png" type="image/png" />
+        <link rel="icon" href="/icon.png" type="image/png" />
         <link rel="viewport" href="width=device-width, initial-scale=1.0" />
 
         <title>{TITLE}</title>
